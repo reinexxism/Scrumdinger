7f77e16fd1a16c2d229eb05ac8b6b0f7e283c250
reinexxism <lucy0805@naver.com>
2023-11-17 18:24:23 +0900

    Modify: make scrums identifiable

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by 최주현 on 11/17/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
